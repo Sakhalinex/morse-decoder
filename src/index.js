c6bee68c9f6650bbd746e1d7a000f3e51c48ce8f
An Dmitriy <dmitrii.7774@gmail.com>
2023-01-24 18:40:44 +1000

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exp = expr.split(''),
+    arr = [],
+    morse = [],
+    current = ''
+
+    // Разбил входящую строку по кодировке для каждой буквы
+    for (let i = 0; i < exp.length / 10; i++) {
+        arr[i] = []
+        for (let j = i * 10; j < (i + 1) * 10; j++) {
+            arr[i].push(exp[j])
+        }
+    }
+    // Ковертирую цифры в символы + формирую новый массив morse
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j+=2) {
+            if (arr[i][j] + arr[i][j + 1] == 10) {
+                current += '.'
+            } else if (arr[i][j] + arr[i][j + 1] == 11) {
+                current += '-'
+            } else if (arr[i][j] == '*') {
+                current += '**'
+            }
+        }
+
+        morse.push(current)
+        current = ''
+    }
+    // 
+    return morse.map(code => code[0] == "*" ? ' ' : MORSE_TABLE[code]).join('')
 }
 
 module.exports = {
